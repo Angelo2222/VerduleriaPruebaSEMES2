a68d9650f504e7e84ff9baf8e9e853d314087e47
angelo marconi <holanda70@live.com.ar>
2023-06-28 16:45:44 -0300

    Agregar archivos de proyecto.

---
+++ Main.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace VerduleriaPruebaSEMES2
+{
+    partial class frmVentas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CoddeVendedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CodProducto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kilos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lblVentas = new System.Windows.Forms.Label();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.lblKilos = new System.Windows.Forms.Label();
+            this.lblFechas = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtKilos = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CoddeVendedor,
+            this.CodProducto,
+            this.Fecha,
+            this.Kilos});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(442, 346);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // CoddeVendedor
+            // 
+            this.CoddeVendedor.HeaderText = "Codigo de Vendedor";
+            this.CoddeVendedor.Name = "CoddeVendedor";
+            // 
+            // CodProducto
+            // 
+            this.CodProducto.HeaderText = "Codigo de Producto";
+            this.CodProducto.Name = "CodProducto";
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha";
+            this.Fecha.Name = "Fecha";
+            // 
+            // Kilos
+            // 
+            this.Kilos.HeaderText = "Kilos";
+            this.Kilos.Name = "Kilos";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(233, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(50, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // lblVentas
+            // 
+            this.lblVentas.AutoSize = true;
+            this.lblVentas.Location = new System.Drawing.Point(9, 15);
+            this.lblVentas.Name = "lblVentas";
+            this.lblVentas.Size = new System.Drawing.Size(40, 13);
+            this.lblVentas.TabIndex = 3;
+            this.lblVentas.Text = "Ventas";
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(177, 15);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(50, 13);
+            this.lblProducto.TabIndex = 4;
+            this.lblProducto.Text = "Producto";
+            // 
+            // lblKilos
+            // 
+            this.lblKilos.AutoSize = true;
+            this.lblKilos.Location = new System.Drawing.Point(12, 53);
+            this.lblKilos.Name = "lblKilos";
+            this.lblKilos.Size = new System.Drawing.Size(29, 13);
+            this.lblKilos.TabIndex = 6;
+            this.lblKilos.Text = "Kilos";
+            // 
+            // lblFechas
+            // 
+            this.lblFechas.AutoSize = true;
+            this.lblFechas.Location = new System.Drawing.Point(177, 53);
+            this.lblFechas.Name = "lblFechas";
+            this.lblFechas.Size = new System.Drawing.Size(37, 13);
+            this.lblFechas.TabIndex = 7;
+            this.lblFechas.Text = "Fecha";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(233, 47);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // txtKilos
+            // 
+            this.txtKilos.Location = new System.Drawing.Point(50, 50);
+            this.txtKilos.Name = "txtKilos";
+            this.txtKilos.Size = new System.Drawing.Size(121, 20);
+            this.txtKilos.TabIndex = 9;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(360, 10);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(105, 60);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // frmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtKilos);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblFechas);
+            this.Controls.Add(this.lblKilos);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.lblVentas);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmVentas";
+            this.Text = "VENTAS";
+            this.Load += new System.EventHandler(this.frmVentas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lblVentas;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CoddeVendedor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodProducto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kilos;
+        private System.Windows.Forms.Label lblKilos;
+        private System.Windows.Forms.Label lblFechas;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtKilos;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VerduleriaPruebaSEMES2
+{
+    public partial class frmVentas : Form
+    {
+        public frmVentas()
+        {
+            InitializeComponent();
+        }
+        public string Conexion = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source = VERDULEROS.mdb";
+
+
+        private void frmVentas_Load(object sender, EventArgs e)
+        {
+            clsBaseDeDatos clsBaseDeDatos = new clsBaseDeDatos();
+            clsBaseDeDatos.CargarTabla(Conexion, "Ventas", dgvVentas);
+            clsBaseDeDatos.CargarProductos(Conexion, "Productos", cmbProducto);
+            clsBaseDeDatos.CargarVendedores(Conexion, "Vendedores", cmbVendedor);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            {
+                clsBaseDeDatos clsBaseDeDatos = new clsBaseDeDatos();
+                clsBaseDeDatos.GrabarVentas(Conexion, "Ventas", Convert.ToInt32(cmbVendedor.SelectedValue), Convert.ToInt32(cmbProducto.SelectedValue), dtpFecha.Value, Convert.ToInt32(numKilos.Value));
+                clsBaseDeDatos.CargarTabla(Conexion, "Ventas", dgvVentas);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
